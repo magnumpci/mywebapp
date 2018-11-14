6b7b867012ed50ea98825fb0947809ce52873463
Sean Drohan <sean.drohan@gmail.com>
2018-11-14 16:52:51 -0500

    New Stuffs

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace mywebapp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Sean!");
             });
         }
     }
